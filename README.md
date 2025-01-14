25fe2932f4eec76cadd328dcd1747baddf7bc062
Luiz <131723272+LzGuimaraes@users.noreply.github.com>
2025-01-14 01:23:07 +0000

    adiciona scripts `lint:check` e `lint:fix`

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # CursoDechamps
+
 CursoDechamps
+
+Comandos de terminal
+clear / ctrl + L
+
+Divisão do React e do Next
--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,11 @@
 function Home() {
-    return <h3> My first page</h3>
+  return <h3> My first page</h3>;
 }
+
+function teste() {
+  console.log("teste");
+}
+
+teste();
 
 export default Home;
